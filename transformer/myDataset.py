acddfcd53187fe71fb975e3e33f203bbd70c7689
lu <st140833@stud.uni-stuttgart.de>
2024-05-15 20:59:21 +0200

    Add classes ImageHODataset, OnlyGazeDataset, MulDataset

--- transformer/myDataset.py
+++ transformer/myDataset.py
@@ -1,98 +1,249 @@
+# -------------------------------------------------------------------
+# Rewrite the dataset class to load the image, gaze and label data
+# Three Classes: ImageGazeDataset, ImageHODataset, OnlyGazeDataset
+# Update from myDataset3.py and myDataset4.py
+# -------------------------------------------------------------------
+
 import os
 import glob
+import time
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
-from PIL import Image
+from torchvision.io import read_image
 import torch
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+
 
 class ImageGazeDataset(Dataset):
+    """Load image, gaze-hand-object features and label data."""
     def __init__(self, image_folder, gaze_folder, label_folder, transform=None):
-        self.clips = {}
+        self.image_folder = image_folder
+        self.gaze_folder = gaze_folder
+        self.label_folder = label_folder
         self.transform = transform
-        
-        clip_paths = glob.glob(os.path.join(image_folder, '*'))
-        
-        for clip_path in clip_paths:
-            clip_name = os.path.basename(clip_path)
-            image_paths = sorted(glob.glob(os.path.join(clip_path, '*.jpg')))
-            if len(image_paths) != 32:
-                # Skip any clip that does not have exactly 32 frames
-                continue  
-
-            gaze_data = []
-            labels = []
-            for path in image_paths:
-                base_name = os.path.splitext(os.path.basename(path))[0]
-
-                # Load gaze data
-                csv_path = os.path.join(gaze_folder, base_name + '.csv')
-                df = pd.read_csv(csv_path)
-                # if df has more than 5 rows, not include head keep first 5 rows
-                if df.shape[0] > 5:
-                    df = df.head(5)
-                gaze_data.append(torch.tensor(df.values, dtype=torch.float32))
-                
-                # Load label data
-                label_pf = pd.read_csv(label_folder, sep=' ', header=None)
-                label = label_pf[label_pf[0] == base_name.split('_')[0]][1].values[0]
-                # labels.append(torch.tensor(label, dtype=torch.int32))
-            
-            # Stack all gaze and label tensors for this clip
-            self.clips[clip_name] = {
-                'images': image_paths,
-                'gaze': torch.stack(gaze_data),
-                # 'label': torch.tensor(labels).unsqueeze(-1)  # Shape (32, 1)
-                # 'label': torch.stack(labels) # Shape (32, 1)
-                'label': torch.tensor(label, dtype=torch.long)
-            }
-            # print(f"images shape is {len(self.clips[clip_name]['images'])}")
-            # print(f"gaze shape is {self.clips[clip_name]['gaze'].shape}")
-            # print(f"label shape is {self.clips[clip_name]['label'].shape}")
-            # print(f"label value is {self.clips[clip_name]['label']}")
-            # print(f"label is {self.clips[clip_name]['label']}")
+        self.label_pf = pd.read_csv(self.label_folder, sep=' ', header=None)
+        self.clips = sorted(glob.glob(os.path.join(image_folder, '*')))
+        # self.clips = [clip for clip in self.clips if len(glob.glob(os.path.join(os.path.dirname(clip), '*.jpg'))) == 32]
 
     def __len__(self):
         return len(self.clips)
 
-    def save_dataset(self, save_path='/scratch/users/lu/msc2024_mengze/dataset/dataset_test_split1.pt'):
-        """
-        Saves the entire dataset as a single .pt file.
+    def __getitem__(self, idx):
+        clip_path = self.clips[idx]
+        image_paths = sorted(glob.glob(os.path.join(clip_path, '*.jpg')))
+        clip_name = os.path.splitext(os.path.basename(clip_path))[0]
+        # base_name = os.path.splitext(os.path.basename(image_path))[0]
+        
+        features = []
 
-        Args:
-            save_path (str): The path to save the dataset file.
-        """
-        all_samples = []
-        for idx in range(len(self)):
-            sample = self.__getitem__(idx)  # Call __getitem__ to get a sample
-            all_samples.append(sample)
+        # Load image
+        # to cuda
+        # image_s = time.time()
+        images = [read_image(image_path) for image_path in image_paths]
+        # to train.py
+        # if self.transform:
+        #     images = [self.transform(image) for image in images]
+        # Shape should be (32, 3, H, W)
+        images_tensor = torch.stack(images)
+        # image_e = time.time()
+        # print(f"load image time is {image_e - image_s}")
 
-        torch.save(all_samples, save_path)
-        print(f"Dataset saved to: {save_path}")
+        # Load gaze data
+        # feature_s = time.time()
+        base_names = [os.path.splitext(os.path.basename(image_path))[0] for image_path in image_paths]
+        for base_name in base_names:
+            csv_path = os.path.join(self.gaze_folder, base_name + '.csv')
+            data = np.loadtxt(csv_path, delimiter=',', skiprows=1)
+            # print(f"data is {data}")
+            # print(f"data shape is {data.shape}")
+            data = torch.tensor(data, dtype=torch.float32)
+            features.append(data)
+        # stack all gaze tensors for this clip 
+        features = torch.stack(features)
+        # feature_e = time.time()
+        # print(f"load gaze time is {feature_e - feature_s}")
+
+        # Load label data
+        # label_s = time.time()
+        label = self.label_pf[self.label_pf[0] == clip_name][1].values[0]
+        # change the number from 1-106 to 0-105 to pass the index of scroes
+        label -= 1
+        label = torch.tensor(label, dtype=torch.long)
+        # label_e = time.time()
+        # print(f"load label time is {label_e - label_s}")
+        # print(f"clip_name is {clip_name} and label is {label}")
+        return {
+            'images': images_tensor,
+            'features': features,
+            'label': label
+        }
+
+
+class ImageHODataset(Dataset):
+    """Load image, hand-object features and label data."""
+    def __init__(self, image_folder, gaze_folder, label_folder, transform=None):
+        self.image_folder = image_folder
+        self.gaze_folder = gaze_folder
+        self.label_folder = label_folder
+        self.transform = transform
+        self.label_pf = pd.read_csv(self.label_folder, sep=' ', header=None)
+        self.clips = sorted(glob.glob(os.path.join(image_folder, '*')))
+        # self.clips = [clip for clip in self.clips if len(glob.glob(os.path.join(os.path.dirname(clip), '*.jpg'))) == 32]
+
+    def __len__(self):
+        return len(self.clips)
 
     def __getitem__(self, idx):
-        clip_name = list(self.clips.keys())[idx]
-        clip_data = self.clips[clip_name]
+        clip_path = self.clips[idx]
+        image_paths = sorted(glob.glob(os.path.join(clip_path, '*.jpg')))
+        clip_name = os.path.splitext(os.path.basename(clip_path))[0]
+        
+        # Load image
+        # image_s = time.time()
+        images = [read_image(image_path).to('cuda') for image_path in image_paths]
+        # to train.py
+        # if self.transform:
+        #     images = [self.transform(image) for image in images]
+        # Shape should be (32, 3, H, W)
+        images_tensor = torch.stack(images)
+        # image_e = time.time()
+        # print(f"load image time is {image_e - image_s}")
 
-        images = [Image.open(path).convert('RGB') for path in clip_data['images']]
-        if self.transform:
-            images = [self.transform(image) for image in images]
-        
-        images_tensor = torch.stack(images)  # Shape should be (32, 3, H, W), H and W depend on the transform
-        
-        sample = {
+        # Load gaze data
+        features = []
+        base_names = [os.path.splitext(os.path.basename(image_path))[0] for image_path in image_paths]
+        for base_name in base_names:
+            csv_path = os.path.join(self.gaze_folder, base_name + '.csv')
+            data = np.loadtxt(csv_path, delimiter=',', skiprows=2)
+            # print(f"data shape is {data.shape}")
+            data = torch.tensor(data, dtype=torch.float32, device='cuda')
+            features.append(data)
+        # stack all gaze tensors for this clip 
+        features = torch.stack(features)
+
+        # Load label data
+        # label_s = time.time()
+        label = self.label_pf[self.label_pf[0] == clip_name][1].values[0]
+        # change the number from 1-106 to 0-105 to pass the index of scroes
+        label -= 1
+        label = torch.tensor(label, dtype=torch.long, device='cuda')
+        # label_e = time.time()
+        # print(f"load label time is {label_e - label_s}")
+        # print(f"clip_name is {clip_name} and label is {label}")
+        return {
             'images': images_tensor,
-            'gaze': clip_data['gaze'],
-            'label': clip_data['label']
+            'features': features,
+            'label': label
         }
-        return sample
 
-# Assuming transforms and paths initialization are correct as per the user's environment
 
+class OnlyGazeDataset(Dataset):
+    """Load image, gaze features and label data."""
+    def __init__(self, image_folder, gaze_folder, label_folder, transform=None):
+        self.image_folder = image_folder
+        self.gaze_folder = gaze_folder
+        self.label_folder = label_folder
+        self.transform = transform
+        self.label_pf = pd.read_csv(self.label_folder, sep=' ', header=None)
+        self.clips = sorted(glob.glob(os.path.join(image_folder, '*')))
+        # self.clips = [clip for clip in self.clips if len(glob.glob(os.path.join(os.path.dirname(clip), '*.jpg'))) == 32]
+
+    def __len__(self):
+        return len(self.clips)
+
+    def __getitem__(self, idx):
+        clip_path = self.clips[idx]
+        image_paths = sorted(glob.glob(os.path.join(clip_path, '*.jpg')))
+        clip_name = os.path.splitext(os.path.basename(clip_path))[0]
+        
+        # Load image
+        # image_s = time.time()
+        images = [read_image(image_path).to('cuda') for image_path in image_paths]
+        # to train.py
+        # if self.transform:
+        #     images = [self.transform(image) for image in images]
+        # Shape should be (32, 3, H, W)
+        images_tensor = torch.stack(images)
+        # image_e = time.time()
+        # print(f"load image time is {image_e - image_s}")
+
+        # Load gaze data
+        features = []
+        base_names = [os.path.splitext(os.path.basename(image_path))[0] for image_path in image_paths]
+        for base_name in base_names:
+            csv_path = os.path.join(self.gaze_folder, base_name + '.csv')
+            data = np.loadtxt(csv_path, delimiter=',', skiprows=1, max_rows=1)
+            print(f"data shape is {data.shape}")
+            data = torch.tensor(data, dtype=torch.float32, device='cuda')
+            features.append(data)
+        # stack all gaze tensors for this clip 
+        features = torch.stack(features)
+
+        # Load label data
+        # label_s = time.time()
+        label = self.label_pf[self.label_pf[0] == clip_name][1].values[0]
+        # change the number from 1-106 to 0-105 to pass the index of scroes
+        label -= 1
+        label = torch.tensor(label, dtype=torch.long, device='cuda')
+        # label_e = time.time()
+        # print(f"load label time is {label_e - label_s}")
+        # print(f"clip_name is {clip_name} and label is {label}")
+        return {
+            'images': images_tensor,
+            'features': features,
+            'label': label
+        }
+    
+
+class MulDataset(Dataset):
+    """Use ThreadPoolExecutor to accelerate the data loading process."""
+    def __init__(self, image_folder, gaze_folder, label_folder, transform=None):
+        self.image_folder = image_folder
+        self.gaze_folder = gaze_folder
+        self.transform = transform
+        self.label_pf = pd.read_csv(label_folder, sep=' ', header=None)
+        self.clips = sorted(glob.glob(os.path.join(image_folder, '*')))
+        self.label_map = {row[0]: row[1] - 1 for _, row in self.label_pf.iterrows()}  # Preload labels and adjust index
+
+    def __len__(self):
+        return len(self.clips)
+
+    def load_images(self, image_paths):
+        return [read_image(image_path) for image_path in image_paths]
+
+    def load_gaze_data(self, csv_path):
+        data = np.loadtxt(csv_path, delimiter=',', skiprows=1)
+        return torch.tensor(data, dtype=torch.float32)
+
+    def __getitem__(self, idx):
+        clip_path = self.clips[idx]
+        image_paths = sorted(glob.glob(os.path.join(clip_path, '*.jpg')))
+        clip_name = os.path.splitext(os.path.basename(clip_path))[0]
+
+        with ThreadPoolExecutor() as executor:
+            images_future = executor.submit(self.load_images, image_paths)
+            base_names = [os.path.splitext(os.path.basename(image_path))[0] for image_path in image_paths]
+            gaze_futures = [executor.submit(self.load_gaze_data, os.path.join(self.gaze_folder, base_name + '.csv')) for base_name in base_names]
+
+            images = images_future.result()
+            features = [gaze_future.result() for gaze_future in gaze_futures]
+
+        images_tensor = torch.stack(images)
+        features = torch.stack(features)
+        label = torch.tensor(self.label_map[clip_name], dtype=torch.long)
+
+        return {
+            'images': images_tensor,
+            'features': features,
+            'label': label
+        }
+    
 
 # # 假定文件夹路径
 # image_folder = '/scratch/users/lu/msc2024_mengze/Frames3/test_split1'
-# gaze_folder = '/scratch/users/lu/msc2024_mengze/Extracted_HOFeatures/test_split1/combined_features'
+# gaze_folder = '/scratch/users/lu/msc2024_mengze/Extracted_HOFeatures/test_split1/combined_features_new'
 # label_folder = '/scratch/users/lu/msc2024_mengze/dataset/test_split12.txt'
 
 # # 图像预处理
@@ -102,7 +253,13 @@ class ImageGazeDataset(Dataset):
 # ])
 
 # # 创建数据集实例
-# dataset = ImageGazeDataset(image_folder, gaze_folder, label_folder, transform=transform)
+# dataset = ImageGazeDataset(image_folder, gaze_folder, label_folder, transform=None)
+# dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 
-# 保存数据集
-# dataset.save_dataset()
+# for data in dataloader:
+#     images, gazes, labels = data['image'], data['gaze'], data['label']
+#     # print(f"images shape is {images.shape}")
+#     # print(f"gaze shape is {gazes.shape}")
+#     # print(f"label shape is {labels.shape}")
+# # 保存数据集
+# # dataset.save_dataset()
